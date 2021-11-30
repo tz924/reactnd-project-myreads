e8c7855d4443236b90dc01f9adf0289e9ed8b1a8
Thomas Zhang <zhj930924@gmail.com>
2021-11-30 03:00:24 -0800

    refactor: refactored search components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-// import * as BooksAPI from './api/BooksAPI'
 import "./App.scss";
 
 export default function BooksApp() {
---
+++ src/components/SearchBooksBar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./SearchBooksBar.scss";
+// import * as BooksAPI from './api/BooksAPI'
+
+export default function SearchBooksBar(props) {
+  return (
+    <div className="search-books-bar">
+      <Link to="/">
+        <button className="close-search">Close</button>
+      </Link>
+      <div className="search-books-input-wrapper">
+        {/*
+        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+        You can find these search terms here:
+        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+        you don't find a specific author or title. Every search is limited by search terms.
+      */}
+        <input type="text" placeholder="Search by title or author" />
+      </div>
+    </div>
+  );
+}
